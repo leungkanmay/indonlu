0ce71d92912de77271d4700bebe34a0f81f22f1b
leungkanmay <88900201+leungkanmay@users.noreply.github.com>
2021-08-16 15:25:07 +0700

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -503,9 +503,9 @@ class EntailmentDataLoader(DataLoader):
 #####
 class DocumentSentimentDataset(Dataset):
     # Static constant variable
-    LABEL2INDEX = {'positive': 0, 'neutral': 1, 'negative': 2}
-    INDEX2LABEL = {0: 'positive', 1: 'neutral', 2: 'negative'}
-    NUM_LABELS = 3
+    LABEL2INDEX = {'positive': 0, 'negative': 1}
+    INDEX2LABEL = {0: 'positive', 1: 'negative'}
+    NUM_LABELS = 2
     
     def load_dataset(self, path): 
         df = pd.read_csv(path)
